809ffe8b1770c94a3515f3c0f0c008b3940ec99a
Phil DeMonaco <pdemon@gmail.com>
2017-08-16 08:04:49 -0400

    Major Rework
    Now does backups to local disks.

--- backup/zfs_backup.sh
+++ backup/zfs_backup.sh
@@ -3,101 +3,160 @@
 #
 # Provides a simple mechanism to backup a pool level
 # snapshot to a series of files on a remote server
-# via SSH
+# via SSH or another local disk
 
 # Commands -----------------------------------------
-awk=$(which awk)
-grep=$(which grep)
-ssh=$(which ssh)
-sort=$(which sort)
-tail=$(which tail)
-uniq=$(which uniq)
-xz=$(which xz)
-zfs=$(which zfs)
+XZ=$(which xz)
+ZFS=$(which zfs)
 
 # Functions ----------------------------------------
 # Validate environment
 check_env() {
-  # Ensure environment is consistent
-  if [[ -z $zfs || -z $xz ]]; then
-    printf "error: zfs and xz are required!\n%s", "${usage}" >&2
-    abort_flag=1
-  # Should have four parameters
-  elif [[ $parm_count -lt 4 ]]; then
-    printf "error: all parameters are mandatory!\n%s", "${usage}" >&2
-    abort_flag=2
-  fi
+    # Ensure the script was run as root
+    if [ "${EUID}" -ne 0 ]; then
+        printf "error: must be run as root!\n%s" "${USAGE}" >&2
+    # Ensure environment is consistent
+    elif [[ -z "${ZFS}" ]]; then
+        printf "error: zfs is required!\n%s" "${USAGE}" >&2
+        ABORT_FLAG=1
+    # If compression is enabled xz is required
+    elif [[ -z "${XZ}" ]]; then
+        printf "error: xz is required for compression!\n%s" "${USAGE}" >&2
+        ABORT_FLAG=1
+    # Check for mandatory parameters
+    elif [[ -z "${POOL}" || -z "${TARGET_PATH}" || \
+        -z "${SNAP_FORMAT}" ]]; then
+        printf "error: -z, -f, and -p are mandatory!\n%s" "${USAGE}" >&2
+        ABORT_FLAG=2
+    # Ensure the path is not in the pool if it is not remote
+    # TODO!!
+    fi
 }
 
 # Find the most recent single
 # snapshot matching $snap_format
 get_snap() {
-  $zfs list -Ht snapshot \
-    | $awk 'BEGIN { FS="\t" } 
+  $ZFS list -Ht snapshot \
+    | awk 'BEGIN { FS="\t" } 
   { 
     split( $1, out, "@" ) 
     print out[2] 
   }' \
-    | $grep $snap_format \
-    | $sort \
-    | $uniq \
-    | $tail -n 1
+    | grep "${SNAP_FORMAT}" \
+    | sort \
+    | uniq \
+    | tail -n 1
 }
 
 # Determines the datasets to be backed up
 get_datasets() {
-  $zfs list -Hd 1 $pool \
-    | $awk '
-  { 
-    num=split( $1, out, "/" )
-    if (num > 1) print $1
-  }'
+    if [[ "${ABORT_FLAG}" -lt 1 ]]; then
+        $ZFS list -Hd 1 "${POOL}" \
+            | awk '
+        { 
+            num=split( $1, out, "/" )
+            if (num > 1) print $1
+        }'
+    fi
 }
 
 # Usage --------------------------------------------
-usage="Simple ZFS backup utility
-usage: ./zfs_backup.sh <snap_format> <pool> <target> <path>
+USAGE="Simple ZFS backup utility
+usage: ${0} -z <pool> -p <target-path> -f <snap-format>
+    -h <remote-host> -c
 
-    snap_format     pattern to match snapshot name
-    pool            zpool containing datasets
-    target          backup target host
-    path            path on backup host
+Mandatory Arguments:
+  -z  <pool>            zpool containing datasets
+  -f  <snap-format>     pattern to match snapshot name
+  -p  <target-path>     path in which to store the backups
+
+Options:
+  -h  <remote-host>     remote host to ssh the backup 
+  -c                    enable compression via xz
 "
 
 # Initialize Variables -----------------------------
-abort_flag=0
-snap_name=""
-hostname=$(hostname)
+ABORT_FLAG=0
+SNAP_NAME=""
+HOSTNAME=$(hostname)
+POOL=""
+SNAP_FORMAT=""
+TARGET_HOST=""
+TARGET_PATH=""
+COMPRESSION_FLAG=0
+
 
 ## Begin Execution ---------------------------------
-# Gather Parameters
-snap_format="$1"
-pool="$2"
-target="$3"
-path="$4"
-parm_count=$#
+
+while getopts "z:p:f:hc" OPT; do
+    case "${OPT}" in
+        z)
+            POOL="${OPTARG}"
+            ;;
+        p)
+            TARGET_PATH="${OPTARG}"
+            ;;
+        f)
+            SNAP_FORMAT="${OPTARG}"
+            ;;
+        h)
+            TARGET_HOST="${OPTARG}"
+            ;;
+        c)
+            COMPRESSION_FLAG=1
+            ;;
+        :)
+            printf "error: option \'%s\' requires an argument.\n%s" \
+                "${OPTARG}" "${USAGE}" >&2
+            ABORT_FLAG=1
+            ;;
+    esac
+done
 
 # Validate the environment
 check_env
 
-# Find a single uniq snapshot
-if [[ $abort_flag -lt 1 ]]; then
-  snap_name=$(get_snap)
+# Perform necessary setup
+if [[ $ABORT_FLAG -lt 1 ]]; then
+    # Find a single unique snapshot
+    SNAP_NAME=$(get_snap)
 
-  # Abort if no snapshot was identified 
-  if [[ -z snap_name ]]; then
-      abort_flag=3
-  fi
+    # Abort if no snapshot was identified 
+    if [[ -z "${SNAP_NAME}" ]]; then
+        ABORT_FLAG=3
+    fi
+  
+    # Ensure the destination directory exists
+    if [[ ! -z "${TARGET_HOST}" ]]; then
+        ssh "${TARGET_HOST}" "mkdir -p ${TARGET_PATH}/${HOSTNAME}"
+    else
+        mkdir -p "${TARGET_PATH}/${HOSTNAME}"
+    fi
 fi
 
-# Ensure the target directory exists
-$ssh $target "mkdir -p ${path}/${hostname}"
 
 # Perform the backup if possible
-while read set_name && [[ $abort_flag -lt 1 ]]; do
-  echo "Storing ${set_name}@${snap_name}"
-  $zfs send -R ${set_name}@${snap_name} \
-    | $ssh $target "cat - > ${path}/${hostname}/${set_name}_${snap_name}.img"
+while read -r SET_NAME && [[ $ABORT_FLAG -lt 1 ]]; do
+    # Calculate the set prefix (remove '/')
+    SET_PREFIX=${SET_NAME//\//_}
+
+    # Build the target command
+    if [[ ! -z "${TARGET_HOST}" ]]; then
+        TARGET_CMD="| ssh \"${TARGET_HOST}\" \"cat - > ${TARGET_PATH}/${HOSTNAME}/${SET_PREFIX}_${SNAP_NAME}.img\""
+    else
+        TARGET_CMD="> \"${TARGET_PATH}/${HOSTNAME}/${SET_PREFIX}_${SNAP_NAME}.img\""
+    fi
+    
+    # Build the send command
+    if [[ ${COMPRESSION_FLAG} -eq 1 ]]; then
+        SEND_CMD="$ZFS send -R \"${SET_NAME}@${SNAP_NAME}\" | $XZ"
+    else
+        SEND_CMD="$ZFS send -R \"${SET_NAME}@${SNAP_NAME}\""
+    fi
+
+    echo "Storing ${SET_NAME}@${SNAP_NAME}"
+    echo "$SEND_CMD $TARGET_CMD"
+    eval "$SEND_CMD $TARGET_CMD"
 done < <(get_datasets)
  
-exit $abort_flag
+exit $ABORT_FLAG
